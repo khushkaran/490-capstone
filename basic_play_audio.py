3436d6b8e1de2ff4a6e5fc407a534c2d5772c196
Thaksha <thaksha.AI@gmail.com>
2024-03-08 20:21:19 -0500

    print unique letter if an rfid tag is scanned

--- basic_play_audio.py
+++ basic_play_audio.py
@@ -1,16 +1,27 @@
 from playsound import playsound
-import os
-import serial
+def rfid():
+    import serial
+    ser = serial.Serial()
+    ser.baudrate = 9600
+    try:
+        ser.port = "/dev/cu.usbmodem142301"
+    except:
+        print("error")
 
-ser = serial.Serial('COM3', 9600, timeout=0)
 
-while 1:
-    x = ser.read()
-    
-    if x == 'aye':
+    ser.open()
+    RFID_Data=ser.readline()
+    if RFID_Data:
+        RFID_Data = RFID_Data.decode()  #Decode arduino Serial
+        RFID_Data = RFID_Data.strip()   #Strip Arduino Data to remove string
+        RFID_Data=int(RFID_Data);       #Convert the Data to Int
+        return(RFID_Data)
+
+
+
+while(True):
+    data = rfid()
+    print(data)
+
+    if data == 942022212782:
         print("hello")
-        playsound('filea.mp3')
-    if x == 'bee':
-        playsound('fileb.mp3')
-    else:
-        pass
