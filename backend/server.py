ccd2b87d8efdb4b96604aecc0d4fbff094f8639a
khushkaran <77179102+khushkaran@users.noreply.github.com>
2024-04-09 18:44:35 -0400

    Call create_app

--- backend/server.py
+++ backend/server.py
@@ -6,10 +6,11 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
-
-
     @app.route('/members')
     def api():
         return jsonify({'data': "Hello World"})
 
     return app
+
+if __name__ == "__main__":
+    create_app()
