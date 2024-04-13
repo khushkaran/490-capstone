c00a1685709a754a24a389949e197c0140dd7700
Thaksha <thaksha.AI@gmail.com>
2024-04-13 19:31:06 -0400

    * Connected python to flask so write whats read from arduino to flask. Right now it gives  the reader and tag the name tag+port_name
    * create a database to store char name, reader and tag
    
    * add functions such as get, put, delete and update
    
    * creates endpoints for getting all characters names, endpoint for getting readers and tags and an endpoint for adding character

--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,129 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort, marshal_with, fields
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
-# how to run it: flask --app server --debug ru
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
 
-    @app.route('/members')
-    def api():
-        return jsonify({'data': "Hello World"})
+# how to run it: flask --app server --debug run
 
-    return app
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
+class CharacterModel(db.Model):
+    name = db.Column(db.String(100), primary_key=True)
+    tag = db.Column(db.String(100), nullable=False)
+    reader = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"Character(name ={name}, tag = {tag}, reader = {reader})"
+
+set_char_args = reqparse.RequestParser()
+set_char_args.add_argument("name", type=str, help="Name of character is needed", required=True)
+set_char_args.add_argument("tag", type=str, help="tag is needed", required=True)
+set_char_args.add_argument("reader", type=str, help="reader is needed", required=True)
+
+update_char_args = reqparse.RequestParser()
+update_char_args.add_argument("name", type=str, help="Name of character is needed", required=True)
+update_char_args.add_argument("tag", type=str, help="tag is needed")
+update_char_args.add_argument("reader", type=str, help="reader is needed")
+
+resource_fields = {
+    'name': fields.String,
+    'tag': fields.String,
+    'reader': fields.String
+}
+
+resource_fields2 = {
+    'tag': fields.String,
+    'reader': fields.String
+}
+
+resource_fields3 = {
+    'name': fields.String
+}
+
+
+class AddChar(Resource):
+
+    @marshal_with(resource_fields)
+    def get(self, name):
+        result = CharacterModel.query.filter_by(name=name).first()
+        if not result:
+            abort(404, message="Could not find character")
+        return result
+    
+   
+    
+    @marshal_with(resource_fields)
+    def put(self, name):
+        args = set_char_args.parse_args()
+        result = CharacterModel.query.filter_by(name=name).first()
+        if result:
+            abort(409, message="Character with name exsist")
+        character = CharacterModel(name=name, tag=args['tag'], reader=args['reader'])
+        db.session.add(character)
+        db.session.commit()
+        return character, 201
+    
+    @marshal_with(resource_fields)
+    def patch(self, name):
+        args =  update_char_args.parse_args()
+        result = CharacterModel.query.filter_by(name=name).first()
+        if not result:
+            abort(404, message="Character doesn't exsist")
+
+        if args["name"]:
+            result.name = args['name']
+
+        if args["tag"]:
+            result.tag = args['tag']
+
+        if args["reader"]:
+            result.name = args['reader']
+        
+        db.session.commit()
+
+        return result
+
+
+    def delete(self, name):
+
+        result = CharacterModel.query.filter_by(name=name).first()
+        if not result:
+            abort(404, message="Character doesn't exsist")
+
+        result.delete()
+        db.session.commit()
+
+        return '', 204
+
+
+
+class GetReaderAndTags(Resource):
+
+    @marshal_with(resource_fields2)
+    def get(self):
+        result = CharacterModel.query.all()
+        if not result:
+            abort(404, message="No characters exsist")
+        
+        return result
+    
+class GetAllChar(Resource):
+
+    @marshal_with(resource_fields3)
+    def get(self):
+        result = CharacterModel.query.all()
+        if not result:
+            abort(404, message="No characters exsist")
+        return result
+    
+api.add_resource(AddChar, "/addChar/<string:name>")
+api.add_resource(GetReaderAndTags, "/getReaderAndTags")
+api.add_resource(GetAllChar, "/getAllChar")
+    
 if __name__ == "__main__":
-    create_app()
+    app.run(debug=True)
--- basic_play_audio.py
+++ basic_play_audio.py
@@ -1,16 +1,17 @@
 from csv import DictReader
 
-from playsound import playsound  # need version 1.2.2
+#from playsound import playsound  # need version 1.2.2
 from serial import Serial
 from queue import Queue
 import threading
 from queue import Empty
+import requests
 
 #reference: https:// stackoverflow.com / questions / 38861980 / attempting - to - read -from-two - serial - ports - at - once
 rfid_reader_queue = Queue(1000)
 
-serA = Serial("/dev/cu.usbmodem142301")
-serB = Serial("/dev/cu.usbmodem142201")
+serA = Serial("/dev/cu.usbmodem141101")
+serB = Serial("/dev/cu.usbmodem141201")
 
 def serial_read(s, port_name):
     while True:
@@ -25,6 +26,7 @@ threadA.start()
 threadB = threading.Thread(target=serial_read, args=(serB, "serB"))
 threadB.start()
 
+BASE = "http://127.0.0.1:5000/"
 
 while True:
 
@@ -32,16 +34,18 @@ while True:
         port_name, tag = rfid_reader_queue.get(True, 1)
         # from csv we find the row that has same port and tag as read and play audio associated with it
 
-        #print(f"Reading from {port_name}: {tag}")
 
-        with open("sound_associations.csv", 'r') as f:
-            dict_reader = DictReader(f)
-            list_of_dict = list(dict_reader)
+        #print(tag+port_name)
+        data = {"name": str(tag+port_name), "tag": tag, "reader": port_name}
+        #print(data)
+        response = requests.put(BASE + "/addChar/" + str(tag + port_name), data)
+        print(response.json())
 
+        input()
+        #addChar/942022212782serA
+        response = requests.get(BASE + "/getAllChar")
+        print(response.json())
 
-        for row in list_of_dict:
-            if row['Tag'] == tag and row['Reader'] == port_name:
-                playsound("./sounds/"+ row['Sound'])
 
 
 
